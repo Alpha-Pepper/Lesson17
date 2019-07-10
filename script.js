54c5fff10377180e74c930131284819dba72d470
unknown <Alpha-Pepper>
2019-07-10 19:20:26 +0300

    homework17

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict"
+
+var seconds = 60;
+var minutes = 60;
+var houres = 3;
+var sum = seconds * minutes * houres;
+console.log(sum);
+
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num ++;
+num --;
+alert(num);
+
+var a = 10;
+var b = 2;
+var c = 5;
+console.log(a + b + c);
+
+var a1 = 17;
+var b1 = 10;
+var c1 = a1 - b1;
+var d = 7;
+var result = c1 + d;
+console.log(result);
+
+var name = 'Алексей';
+console.log('Привет,' + ' ' + name + '!');
